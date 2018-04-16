50e7986d17fe9c216875c5f6aa718f4def626cbf
erik <zerpaerik@gmail.com>
2018-04-16 09:58:18 -0300

    cambios palabras que cambian

--- app/Http/Controllers/Admin/DesinenciaController.php
+++ app/Http/Controllers/Admin/DesinenciaController.php
@@ -33,6 +33,8 @@ class DesinenciaController extends Controller
 			if (!array_key_exists($DesIdx, $data[$key])) continue;
 		
 			$desinencia = str_replace(" ", "", $data[$key][$DesIdx]);
+			$desinencia = str_replace("[", '<b class="dc">', $data[$key][$DesIdx]);
+			$desinencia = str_replace("]", '</b>', $data[$key][$DesIdx]);
 
 			$insert = [
 				"desinencia" => utf8_encode($desinencia),
